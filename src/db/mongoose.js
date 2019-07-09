eac3c596542cfa7c8f755d2e941d94d9ebfa5637
ahashan-LS <ahashan@linkstaff.co.jp>
2019-07-09 12:25:30 +0600

    Added Cloud Mongodb Connection String

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/blog-api', {
+mongoose.connect('mongodb+srv://root:root@cluster0-9fexc.mongodb.net/mern?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
